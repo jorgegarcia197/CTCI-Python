32b2b7eba56a6b4b1ad49936e74e384070b81c6b
Jorge Eduardo García Serna <jorge.garcias@softtek.com>
2022-06-13 19:25:41 -0500

    improved balanced brackets function

--- AlgoExpert/arrays/balanced_brackets.py
+++ AlgoExpert/arrays/balanced_brackets.py
@@ -1,32 +1,52 @@
 from collections import deque
+
+from grpc import channel_ready_future
+
+
 def balancedBrackets(string):
-	# Write your code here.
-	brackets = {
-		"(":")",
-		"{": "}",
-		"[": "]"
-	}
-	queue = deque()
-	left_brackets = ["(","[","{"]
-	right_brackets = [")","]","}"]
-	for bracket in string: 
-		if bracket in left_brackets:
-			queue.append(bracket)
-		elif bracket in right_brackets: 
-			if len(queue) > 0:
-				last_left = queue.pop()
-				if bracket != brackets[last_left]:
-					return False
-			else:
-				return False
-		else:
-			continue
-	return len(queue) == 0 
-			
+    # Write your code here.
+    brackets = {
+        "(": ")",
+        "{": "}",
+        "[": "]"
+    }
+    queue = deque()
+    left_brackets = ["(", "[", "{"]
+    right_brackets = [")", "]", "}"]
+    for bracket in string:
+        if bracket in left_brackets:
+            queue.append(bracket)
+        elif bracket in right_brackets:
+            if len(queue) > 0:
+                last_left = queue.pop()
+                if bracket != brackets[last_left]:
+                    return False
+            else:
+                return False
+        else:
+            continue
+    return len(queue) == 0
+
+
+def isValid(string):
+    brackets = {
+        "(": ")",
+        "{": "}",
+        "[": "]"
+    }
+    stack = []
+    for ch in string:
+        if brackets.get(ch):
+            stack.append(ch)
+        else:
+            last_opened_bracket = stack.pop()
+            if brackets[last_opened_bracket] != ch:
+                return False
+    return len(stack) == 0
 
 
 if __name__ == "__main__":
-	input_dict = {
-		"string": "([])(){}(())()()"
-	}
-	print(balancedBrackets(input_dict["string"]))
+    input_dict = {
+        "string": "([])(){}(())()()"
+    }
+    print(isValid(input_dict["string"]))
